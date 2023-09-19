2cb5e920ad9ffd31f54ed5bb3649741f37044e85
ignaciocampillosserrano <ignacio.campillos.serrano@cieep.com>
2023-09-19 12:47:55 +0200

    Initial commit

---
+++ app/src/main/java/ignacio/campillos/androidstudio_recogerinformacion_02/Modelos/Usuario.java
@@ -0,0 +1,35 @@
+package ignacio.campillos.androidstudio_recogerinformacion_02.Modelos;
+
+public class Usuario {
+    private String email;
+    private String password;
+
+    public Usuario() {
+    }
+
+    public Usuario(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return " Email: " + email + "| Password: " + password;
+    }
+}
